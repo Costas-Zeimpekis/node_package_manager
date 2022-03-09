830646142b3f47a06b4a4d8bc5359a4c7a761301
Costas-Zeimpekis <morpheustudio@gmail.com>
2022-03-09 17:47:17 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const _ = require("underscore"); // underscore/index.js
+
+//Steps of require search path
+// Core module
+// File or folder
+// Module
+
+const result = _.contains([1, 2, 3], 2);
+console.log("Result:", result);
